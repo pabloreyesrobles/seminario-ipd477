6f79457ab0308cc398d9966ec40ae003846f175a
Pablo Reyes Robles <pabloreyes500@gmail.com>
2021-12-20 19:12:16 -0300

    minor fix

--- extract_features.py
+++ extract_features.py
@@ -161,7 +161,7 @@ num_threads = int(os.cpu_count() / 2)
 with warnings.catch_warnings():
   warnings.filterwarnings('ignore')
   with Pool(num_threads) as pool:
-    pop_feat = list(tqdm(pool.imap(get_features, s_paths[:2]), total=len(s_paths[:2])))
+    pop_feat = list(tqdm(pool.imap(get_features, s_paths), total=len(s_paths)))
 
 df = pd.DataFrame(pop_feat)
 df['class'] = classes
