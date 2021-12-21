e8ba0fdff18c091c54913f9deae85c3a1dab9552
Pablo Reyes Robles <pabloreyes500@gmail.com>
2021-12-21 17:12:23 -0300

    error strict handling

--- features.py
+++ features.py
@@ -1,3 +1,4 @@
+from logging import raiseExceptions
 import numpy as np
 import struct
 import pywt
@@ -53,7 +54,7 @@ def get_features(path):
   mIMFs = np.mean(np.split(IMFs[:-1, :-q], 10, axis=1), axis=0)
   mt = np.split(t[:-q], 10)[0]
 
-  # Time features
+  # Time features mapping
   zc_map = np.array([0, 1, 2, 5, 6, 7, 8, 9, 10, 11])
   ssc_map = np.array([0, 1, 4, 5])
 
@@ -66,41 +67,8 @@ def get_features(path):
             7: np.array([1]),
             8: np.array([2]),
             12: np.array([0])}
-
-  wl = np.sum(np.abs(np.diff(mIMFs)), axis=1)
-  zc = np.sum(np.abs(np.diff(np.sign(mIMFs), axis=1)) == 2, axis=1)[zc_map]
-  ssc = np.sum(np.abs(np.diff(np.sign(np.diff(mIMFs, axis=1)), axis=1)) == 2, axis=1)[ssc_map]
-  rms = np.sqrt(np.mean(mIMFs ** 2, axis=1))
-
-  ar = []
-  for i, imf in enumerate(mIMFs):
-    # print(f'Running imf_{i+1:d}:')
-    if i not in ar_map:
-      # print('\tskipping')
-      continue
-    else:
-      try:
-        _ar = ARIMA(imf, order=[6, 0, 0]).fit(method='yule_walker').polynomial_ar[1:][ar_map[i]]
-      except:
-        _ar = np.zeros(6)[ar_map[i]]
-      ar.append(_ar)
-  ar = np.concatenate(ar)
-      
-  mav = np.mean(np.abs(mIMFs), axis=1)
-  iav = np.sum(np.abs(mIMFs), axis=1)
-
-  f_feat = np.concatenate([wl, zc, ssc, rms, ar, mav, iav])
-
-  # Freq - hilbert features
-  analytic_sig = np.zeros_like(mIMFs, dtype=np.complex64)
-
-  mif = np.zeros(nIMFs)
-  mfd = np.zeros(nIMFs)
-  smpds = np.zeros(nIMFs)
-  amb = np.zeros(nIMFs)
-  fmb = np.zeros(nIMFs)
-  ppsd = np.zeros(nIMFs)
-
+  
+  # Freq features mapping
   amb_map = np.arange(5)
   fmb_map = np.arange(4)
   smpds_map = np.array([0, 1, 2, 3, 4, 7])
@@ -108,33 +76,72 @@ def get_features(path):
   mif_map = np.array([0, 1, 2, 3, 5])
   ppsd_map = np.array([0, 3, 7])
 
-  for i, imf in enumerate(mIMFs):
-    # no queda claro en el paper. Revisar cálculo de amplitud, fase y frecuencia
-    # ver https://ccrma.stanford.edu/~jos/st/Analytic_Signals_Hilbert_Transform.html
-    analytic_sig = signal.hilbert(imf)
-    amps = np.abs(analytic_sig)
-    phases = np.angle(analytic_sig)
-    
-    freqs = np.zeros_like(phases)
-    freqs[1:] = np.diff(phases)
-    
-    psd = freqs ** 2 / (2 * np.pi)
-    
-    mif[i] = np.sum(freqs * amps ** 2) / np.sum(amps ** 2)
-    mfd[i] = np.sum(np.diff(freqs)) / (freqs.shape[0])
-    smpds[i] = np.sum(np.arange(1, psd.shape[0] + 1) * psd)
-    amb[i] = np.sum(np.diff(amps ** 2)) / np.sum(psd[:-1]) # así sale en el paper (?)
-    fmb[i] = np.sum((freqs - mif[i]) * amps ** 2) / np.sum(psd)
-    ppsd[i] = np.max(psd)
-      
-  mif = mif[mif_map]
-  mfd = mfd[mfd_map]
-  smpds = smpds[smpds_map]
-  amb = amb[amb_map]
-  fmb = fmb[fmb_map]
-  ppsd = ppsd[ppsd_map]
+  try:
+    wl = np.sum(np.abs(np.diff(mIMFs)), axis=1)
+    zc = np.sum(np.abs(np.diff(np.sign(mIMFs), axis=1)) == 2, axis=1)[zc_map]
+    ssc = np.sum(np.abs(np.diff(np.sign(np.diff(mIMFs, axis=1)), axis=1)) == 2, axis=1)[ssc_map]
+    rms = np.sqrt(np.mean(mIMFs ** 2, axis=1))
 
-  t_feat = np.concatenate([mif, mfd, smpds, amb, fmb, ppsd])
+    ar = []
+    for i, imf in enumerate(mIMFs):
+      # print(f'Running imf_{i+1:d}:')
+      if i not in ar_map:
+        # print('\tskipping')
+        continue
+      else:
+        try:
+          _ar = ARIMA(imf, order=[6, 0, 0]).fit(method='yule_walker').polynomial_ar[1:][ar_map[i]]
+        except:
+          _ar = np.zeros(6)[ar_map[i]]
+        ar.append(_ar)
+    ar = np.concatenate(ar)
+        
+    mav = np.mean(np.abs(mIMFs), axis=1)
+    iav = np.sum(np.abs(mIMFs), axis=1)
+
+    f_feat = np.concatenate([wl, zc, ssc, rms, ar, mav, iav])
+
+    # Freq - hilbert features
+    analytic_sig = np.zeros_like(mIMFs, dtype=np.complex64)
+
+    mif = np.zeros(nIMFs)
+    mfd = np.zeros(nIMFs)
+    smpds = np.zeros(nIMFs)
+    amb = np.zeros(nIMFs)
+    fmb = np.zeros(nIMFs)
+    ppsd = np.zeros(nIMFs)
+
+    for i, imf in enumerate(mIMFs):
+      # no queda claro en el paper. Revisar cálculo de amplitud, fase y frecuencia
+      # ver https://ccrma.stanford.edu/~jos/st/Analytic_Signals_Hilbert_Transform.html
+      analytic_sig = signal.hilbert(imf)
+      amps = np.abs(analytic_sig)
+      phases = np.angle(analytic_sig)
+      
+      freqs = np.zeros_like(phases)
+      freqs[1:] = np.diff(phases)
+      
+      psd = freqs ** 2 / (2 * np.pi)
+      
+      mif[i] = np.sum(freqs * amps ** 2) / np.sum(amps ** 2)
+      mfd[i] = np.sum(np.diff(freqs)) / (freqs.shape[0])
+      smpds[i] = np.sum(np.arange(1, psd.shape[0] + 1) * psd)
+      amb[i] = np.sum(np.diff(amps ** 2)) / np.sum(psd[:-1]) # así sale en el paper (?)
+      fmb[i] = np.sum((freqs - mif[i]) * amps ** 2) / np.sum(psd)
+      ppsd[i] = np.max(psd)
+        
+    mif = mif[mif_map]
+    mfd = mfd[mfd_map]
+    smpds = smpds[smpds_map]
+    amb = amb[amb_map]
+    fmb = fmb[fmb_map]
+    ppsd = ppsd[ppsd_map]
+
+    t_feat = np.concatenate([mif, mfd, smpds, amb, fmb, ppsd])
+  
+  except:
+    features = np.empty(103)[:] = np.nan
+    return features
 
   features = np.concatenate([f_feat, t_feat])
 
